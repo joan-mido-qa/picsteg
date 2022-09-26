4f2157af48471d52c0b107ee9f2654561f5cde8d
Joan <joan.flotats@midokura.com>
2022-09-26 18:00:52 +0200

    feat(cli): improve cli help

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use utils::{decode_image, encode_image, open_image, open_secret};
 #[clap(author, version, about, long_about = None)]
 #[clap(propagate_version = true)]
 struct Cli {
-    #[clap(short, long)]
+    #[clap(short, long, help="Number of bits to encode or decode per byte. Default: 1")]
     bits: Option<i8>,
 
     #[clap(subcommand)]
@@ -20,14 +20,21 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Command {
+    #[clap(about = "Encode an image with a secret")]
     Encode {
-        input_path: std::path::PathBuf,
-        secret_path: std::path::PathBuf,
-        output_path: std::path::PathBuf,
+        #[clap(short, long, help = "Path to an encodable image")]
+        image: std::path::PathBuf,
+        #[clap(short, long, help = "Path to the secret to be encoded")]
+        secret: std::path::PathBuf,
+        #[clap(short, long, help = "Output path of the encoded image")]
+        output: std::path::PathBuf,
     },
+    #[clap(about = "Decode a secret from an image")]
     Decode {
-        image_path: std::path::PathBuf,
-        secret_path: std::path::PathBuf,
+        #[clap(short, long, help = "Path to an encoded image")]
+        image: std::path::PathBuf,
+        #[clap(short, long, help = "Output path of the decoded secret")]
+        output: std::path::PathBuf,
     },
 }
 
@@ -38,32 +45,32 @@ fn main() {
 
     match &cli.command {
         Command::Encode {
-            input_path,
-            secret_path,
-            output_path,
+            image,
+            secret,
+            output,
         } => {
-            if output_path.extension().unwrap() != "png" {
+            if output.extension().unwrap() != "png" {
                 panic!("Image must be saved with PNG format.");
             }
 
-            let mut image: RgbImage = open_image(input_path);
+            let mut image: RgbImage = open_image(image);
 
-            let secret: String = open_secret(secret_path);
+            let secret: String = open_secret(secret);
 
             image = encode_image(image, secret, bits);
 
-            if let Err(error) = image.save(output_path) {
+            if let Err(error) = image.save(output) {
                 panic!("Encoded image could not be saved: {}", error)
             }
         }
         Command::Decode {
-            image_path,
-            secret_path,
+            image,
+            output,
         } => {
-            let image: RgbImage = open_image(image_path);
+            let image: RgbImage = open_image(image);
             let secret: String = decode_image(image, bits);
 
-            match File::create(secret_path) {
+            match File::create(output) {
                 Ok(mut file) => {
                     if let Err(error) = write!(file, "{}", secret) {
                         panic!("Secret could not be written: {error}")
